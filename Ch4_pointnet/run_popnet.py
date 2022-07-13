c65f6c6f4f9272340afa410f6388600a416851fb
kaeldai <kael.dai@gmail.com>
2022-07-13 12:45:00 -0700

    Updating Ch4, config files and images

--- Ch4_pointnet/run_popnet.py
+++ Ch4_pointnet/run_popnet.py
@@ -1,8 +1,19 @@
+import sys
+import os
+
 from bmtk.simulator import popnet
 
-configure = popnet.config.from_json('config.simulation_popnet.json')
-configure.build_env()
 
-network = popnet.PopNetwork.from_config(configure)
-sim = popnet.PopSimulator.from_config(configure, network)
-sim.run()
+def main(config_file):
+    configure = popnet.config.from_json(config_file)
+    configure.build_env()
+    network = popnet.PopNetwork.from_config(configure)
+    sim = popnet.PopSimulator.from_config(configure, network)
+    sim.run()
+
+
+if __name__ == '__main__':
+    if __file__ != sys.argv[-1]:
+        main(sys.argv[-1])
+    else:
+        main('/home/kael/Workspace/bmtk_workshop_2022/Ch4_pointnet/config.popnet.json')
